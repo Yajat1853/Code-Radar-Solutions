223117c017e6f0601d70f9b0014f5350572d387e
Yajat1853 <yajat1853.becse24@chitkara.edu.in>
2025-02-07 11:06:36 +0530

    Solution to Perform Bitwise One's Complement

---
+++ Perform Bitwise One's Complement@@67882a731b4392d8fc1577e7/code.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int a, one compliment;
+    scanf("%d",a);
+    one compliment = ~a;
+    printf("%d",one compliment);
+    return 0;
+}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
