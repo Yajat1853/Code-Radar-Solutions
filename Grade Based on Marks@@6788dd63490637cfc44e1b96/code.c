b0fe814bd767a76c546e288b03eb8a1cfa7a3192
Yajat Dhiman <yajat7498@gmail.com>
2025-02-14 17:18:53 +0530

    Solution to Grade Based on Marks

--- Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -11,7 +11,7 @@ int main() {
         printf("B");
     } else if (n >= 70 && n < 80){
         printf("C");
-    } eles if (n >= 60 && n < 70){
+    } else if (n >= 60 && n < 70){
         printf("D");
     } else if (n < 60){
         printf("F");
