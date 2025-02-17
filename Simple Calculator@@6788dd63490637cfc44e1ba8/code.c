69e184d737cac8554724b78d71d8598137423451
Yajat Dhiman <yajat7498@gmail.com>
2025-02-17 21:53:54 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -16,7 +16,7 @@ int main() {
     } 
     else if(c == '/'){
         if(b == 0){
-            printf("Error");
+            printf("error");
         }
         else{
             printf("%d",a/b);
