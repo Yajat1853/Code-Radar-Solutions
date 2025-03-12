c7860a3f967274163a39ee2ab39d37ca631fdf27
Yajat Dhiman <yajat7498@gmail.com>
2025-03-12 18:51:39 +0530

    Solution to Find the Frequency of Elements in an Array

--- Find the Frequency of Elements in an Array@@676c02002dbc8c3e7a280b73/code.c
+++ Find the Frequency of Elements in an Array@@676c02002dbc8c3e7a280b73/code.c
@@ -2,13 +2,11 @@
 
 int main() {
     int n;
+    int count = 0;
     scanf("%d",&n);
     int arr[n];
     for(int i = 0; i < n; i++){
         scanf("%d",&arr[i]);
-    }
-    for(int i = 0 ; i<n; i++){
-        int count_i = 0;
     }
     for(int i ; i < n; i++){
         if (arr[i] == arr[i]){
